2213b801fca8cbf19d2b0754266a47cfe076d4e1
haram <yusuif@riseup.net>
2020-04-18 02:14:04 +0200

    Update mem_scan.hpp

--- watermark_disabler/mem_scan.hpp
+++ watermark_disabler/mem_scan.hpp
@@ -8,7 +8,7 @@ namespace impl
 	inline bool search_for_signature_helper( const uint8_t* data, const uint8_t* signature, const char* mask )
 	{
 		// check if page is correct & readable (this internally checks PTE, PDE, ...)
-		if ( !FN( MmIsAddressValid )( const_cast< uint8_t* >( data ) ) )
+		if ( !MmIsAddressValid( const_cast< uint8_t* >( data ) ) )
 			return false;
 
 		// iterate through validity of the mask (mask & signature are equal
